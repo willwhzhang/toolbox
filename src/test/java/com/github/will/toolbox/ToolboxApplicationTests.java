4906cbeda454d4b14f124cbb33ca697be1a72708
zhangweihao <zhangweihao@yy.com>
2021-09-26 21:39:33 +0800

    init project

---
+++ src/test/java/com/github/will/toolbox/ToolboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.will.toolbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
